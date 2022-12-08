0fc147c079f1403fc3a9475a61ecd24af4ef9305
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 11:22:42 +0300

    hero operations

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,22 @@
 class HeroPowersController < ApplicationController
+    # skip_before_action :verify_authenticity_token
+    rescue_from ActiveRecord::RecordInvalid, with: :render_errors
+
+    def index 
+        render json: HeroPower.all 
+    end
+    
+    def create
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: hero_power.hero, serializer: HeroPowerSerializer
+    end
+
+    private
+    
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+    def render_errors(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
