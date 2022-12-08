69d460d08bd10b3b88eb4d2099d2fdb5042942d1
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 11:24:12 +0300

    include validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,2 +1,7 @@
 class HeroPower < ApplicationRecord
+        belongs_to :hero
+        belongs_to :power
+
+        validates :strength, inclusion: { in: %w(Strong Weak Average),
+            message: "%{value} is not a valid strength category." }
 end
