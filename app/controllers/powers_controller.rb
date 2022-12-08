4e6b60bcfac7fdc3407759aa154db989aaaa26f0
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 11:23:16 +0300

    debugging operations

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,18 +1,36 @@
 class PowersController < ApplicationController
-    before :set_power, only: %i[ show update ]
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_message
 
     def index
-        @powers = Power.all
-    
-        render json: @powers
+        render json: Power.all, status: :ok
     end
 
     def show
-        render json: @power
+        power = find_power
+        render json: power
+    end
+
+    def update
+        power = find_power
+        power.update!(power_params)
+        render json: power, status: :accepted
     end
 
     private
-    def set_power
-        @power = Power.find(params[:id])
+    def find_power
+        Power.find_by!(id: params[:id])
+    end
+    
+    def power_params
+        params.permit(:name, :description)
+    end 
+
+    def record_not_found
+        render json: {error:"Power not found"}, status: :not_found
+    end
+
+    def invalid_message invalid
+        render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
