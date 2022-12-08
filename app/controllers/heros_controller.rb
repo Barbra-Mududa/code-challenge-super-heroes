82fa596c4e4814a0323f1e8f60b8a857f76cf9e4
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 09:41:11 +0300

    crud operations working

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,15 +1,20 @@
 class HerosController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     def index
         render json: Hero.all 
     end
 
     def show 
-
+        hero = find_hero
+        render json: hero, status: :ok 
     end
 
     private 
+    def find_hero
+        Hero.find(params[:id])
+    end
+
     def render_not_found 
         render json: { error: "Hero not found" }, status: :not_found
     end
