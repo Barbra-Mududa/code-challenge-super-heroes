62d01b14d39751f481ddb9d5cb0837fd04b82643
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 09:54:02 +0300

    authenticating of power-feature

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,18 @@
 class PowersController < ApplicationController
+    before :set_power, only: %i[ show update ]
+
+    def index
+        @powers = Power.all
+    
+        render json: @powers
+    end
+
+    def show
+        render json: @power
+    end
+
+    private
+    def set_power
+        @power = Power.find(params[:id])
+    end
 end
