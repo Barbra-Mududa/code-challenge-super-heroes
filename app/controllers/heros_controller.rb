6a8de38877a90c473d9413416763071268ace114
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-12-08 09:19:13 +0300

    error handling message

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,16 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Hero.all 
+    end
+
+    def show 
+
+    end
+
+    private 
+    def render_not_found 
+        render json: { error: "Hero not found" }, status: :not_found
+    end
 end
